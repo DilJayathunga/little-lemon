6cc807587429920109cd76ed75c40d9c595c4cbc
Dilhari Jayathunga <dilhari@n-ach.com>
2023-02-09 16:34:31 +0530

    HTML document setting and adding meta tags

---
+++ src/components/Footer.js
@@ -0,0 +1,63 @@
+
+const Footer = () => {
+    return (
+        <footer>
+            <div style={{display: 'flex'}}>
+                <img src="./Logo.svg" alt="logo" />
+                <div>
+                    <h4>Doormat Navigation</h4>
+                    <ul>
+                        <li>
+                            <a>Home</a>
+                        </li>
+                        <li>
+                            <a>About</a>
+                        </li>
+                        <li>
+                            <a>Menu</a>
+                        </li>
+                        <li>
+                            <a>Reservations</a>
+                        </li>
+                        <li>
+                            <a>Order Online</a>
+                        </li>
+                        <li>
+                            <a>Login</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Contact</h4>
+                    <ul>
+                        <li>
+                            <a>Address</a>
+                        </li>
+                        <li>
+                            <a>Phone Number</a>
+                        </li>
+                        <li>
+                            <a>Email</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Social Media Links</h4>
+                    <ul>
+                        <li>
+                            <a>Address</a>
+                        </li>
+                        <li>
+                            <a>Phone Number</a>
+                        </li>
+                        <li>
+                            <a>Email</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+
+const Header = () => {
+    return (
+        <header>
+            <head>
+                <title>Little Lemon</title>
+                <meta name="description" content="We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist."/>
+                <meta name="og:title" content="Little Lemon"/>
+                <meta name="og:description" content="We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist."/>
+                <meta name="og:image" content="./public/Logo.svg"/>
+            </head>
+            <img src="./Logo.svg" alt="logo" />
+        </header>
+    );
+}
+
+export default Header;
